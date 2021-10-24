8fd61775bc056a9ab1a4ceff359855a356ea6076
devops <devops@frcloud3.fast-report.com>
2021-10-24 15:16:02 -0400

    Just start operating as expected

---
+++ 10-monitoring-04-elk/help/delete_all_indexes.sh
@@ -0,0 +1,1 @@
+ curl -X DELETE -u elastic:netology 'http://localhost:9200/_all'
--- 10-monitoring-04-elk/help/pinger/run.py
+++ 10-monitoring-04-elk/help/pinger/run.py
@@ -4,9 +4,19 @@ import logging
 import random
 import time
 
+
+logging.basicConfig(filename='../logs/pinger.log',
+                            filemode='a',
+                            format='%(asctime)s pinger %(levelname)s: %(message)s',
+#                            format='%(asctime)s,%(msecs)d pinger %(levelname)s: %(message)s',
+#                            format='%(asctime)s pinger %(levelname)s %(message)s',
+#                            datefmt='%H:%M:%S',
+                            datefmt='%b %d %H:%M:%S',
+                            level=logging.INFO)
+
 while True:
 
-    number = random.randrange(0, 4)
+    number = random.randrange(0, 10)
 
     if number == 0:
         logging.info('Hello there!!')
@@ -16,5 +26,13 @@ while True:
         logging.error('OH NO!!!!!!')
     elif number == 3:
         logging.exception(Exception('this is exception'))
+    elif number == 4:
+        logging.error(Exception('Host not found'))
+    elif number == 5:
+        logging.warning(Exception('Opertaion timeout'))
+    elif number == 6:
+        logging.exception('User not found')
+    elif number == 7:
+        logging.info('Random number works')
 
-    time.sleep(1)
+    time.sleep(3)
